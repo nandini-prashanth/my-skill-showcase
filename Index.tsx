b5bd8a09c62be1b46b8ca10ead49275c1f5a307d
Nandini Prashanth <40294424+nandini-prashanth@users.noreply.github.com>
2026-04-07 22:16:59 +0530

    Removed Projects import and component usage from Index.tsx

---
+++ Index.tsx
@@ -0,0 +1,9 @@
+// Removed Projects import and component usage
+
+import React from 'react';
+
+const Index = () => {
+    return <div>Welcome to the Skill Showcase!</div>;
+};
+
+export default Index;
